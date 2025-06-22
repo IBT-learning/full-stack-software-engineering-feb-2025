10fd62435db0b58846b996e5665735ce06c13e2d
edemaukabi <edemaukabi@gmail.com>
2025-06-22 20:26:41 +0100

    class lessons

--- 2-javascript/oop.js
+++ 2-javascript/oop.js
@@ -59,26 +59,43 @@ class User {
     constructor(name, state){
         this.name = name;
         this.state = state;
+        // this.size = 7
     }
     greet(){
         return "hello"
     }
+    #helper() {
+      return 3 + 4
+    }
     register(){
         return `${this.name} is registered successfully!!`
     }
+    getHelper(){
+      return this.#helper()
+    }
     login(){
-        return `${this.name} is logged in successfully!!`
+        return `${this.name} is logged in successfully!! ${this.#helper()}`
     }
     logout(){
         return `Bye! ${this.name}, See you some other time`
     }
+    static describe(){
+      return "I am from User class"
+    }
 }
 
 class Admin extends User{
-    // constructor(name, state){
-    //     this.name = name;
-    //     this.state = state
-    // }
+    constructor(name, state, isAdmin){
+        super(name, state)
+        this.isAdmin = isAdmin
+        this.hi = "hello"
+        this.accessKey = "89065"
+    }
+    login(){
+      if(this.accessKey === "89066"){
+        return super.login()
+      } else return "cannot log in"
+    }
     createUser(name, state){
         const user = new User(name, state)
         return user
@@ -86,20 +103,27 @@ class Admin extends User{
 }
 
 const vincent = new User("Vincent", "Lagos")
+// console.log(new Array(3,5,89))
 const Adaobi = new User("Adaobi", "Ogun")
 const Joshua = new User("Joshua", "Ekiti")
 const Peter = new User("Peter", "Abuja")
 const Ayo = new User("Ayo", "Kebbi")
 
-const Daniel = new Admin("Daniel", "Rivers")
+// console.log(User.describe())
+
+// User.describe()
+
+const Daniel = new Admin("Daniel", "Rivers", true)
+console.log(Daniel.login())
+// console.log(Daniel.state)
 
 
 // console.log(Ayo.createUser())
 
-console.log(Daniel.login())
+// console.log(Daniel.login())
 const Favour = Daniel.createUser("Favour", "Bayelsa")
 
-console.log(Favour.login())
+// console.log(Favour.login())
 
 
 
